9e6a5adab0e0bb79848aea1d409b4ab30a95482a
williamcyau <93053771+williamcyau@users.noreply.github.com>
2026-01-19 20:00:30 +0000

    Deploying to gh-pages from @ williamcyau/williamcyau.github.io@4505ee97b8ee689dfe7f2a5c305c5c0320800674 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -452,25 +452,11 @@ ninja.data = [{
           window.open("https://github.com/williamcyau", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/williamcyau", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
